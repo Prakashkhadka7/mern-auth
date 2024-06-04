8f174392241ee5362a68f529a3445bef17a3540a
prakashkhadka663 <prakash@rippey.ai>
2024-06-04 16:12:23 +0545

    added header and routes configuration to mern project.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div>
+      <div className='bg-slate-200'>
+        <div className='flex justify-between p-4'>
+            <h1 className='font-bold'>Auth App</h1>
+            <ul className='flex gap-4'>
+              <Link to='/'><li >Home</li></Link>
+              <Link to='/about'><li >About</li></Link>
+              <Link to='/sign-in'><li >Sign In</li></Link>
+            </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
