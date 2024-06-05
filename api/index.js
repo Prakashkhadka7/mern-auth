b02376e5d5a753b4ff53b8079b4de509519fd063
prakashkhadka663 <prakash@rippey.ai>
2024-06-06 00:44:14 +0545

    created test api routes.

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import donenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+
 
 donenv.config();
-
+const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO).then(()=> {
     console.log('Connected to MongoDB');
 }).catch((error)=> {
     console.log(error);
 });
 const app = express();
-app.listen(()=> {
+app.listen(port ,()=> {
     console.log('Server listening on port 3000!')
 })
+
+
+app.use("/api/user", userRoutes)
