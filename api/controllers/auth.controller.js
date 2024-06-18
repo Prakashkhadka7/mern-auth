bd696f56d0b42fdaed8c60224e58813c615718d2
prakashkhadka7 <prakashkhadka663@gmail.com>
2024-06-19 00:06:40 +0545

    Added signin routes and controller and working api from BE

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = (async(req, res,next) => {
 console.log(req.body);
@@ -18,4 +19,32 @@ try
    next(errorHandler(500,"User already exists"));
 }
     
-}); 
+});
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid credentials"));
+    }
+    const token = jwt.sign(
+      {
+        _id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: hashedPassword, ...others } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    res
+      .cookie("access_token", token, { httpOnly: true ,expires: expiryDate})
+      .status(200)
+      .json(others);
+  } catch (error) {
+    next(error);
+  }
+};
